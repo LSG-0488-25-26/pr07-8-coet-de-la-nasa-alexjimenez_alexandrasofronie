887a8f9661ddd0de1351deb660a616b6338cc943
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2026-01-14 12:24:45 +0100

    Declaramos el modelo de Pokemon mediante un data class con sus parámetros

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/model/Pokemon.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/model/Pokemon.kt
@@ -1,2 +1,10 @@
 package com.example.pr06_lazycomponents.model
 
+import androidx.annotation.DrawableRes
+
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val type: String,
+    @DrawableRes val image: Int
+)
