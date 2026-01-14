ea68fe5ad31f9c963bd2f41e0f642792841fd6a6
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2026-01-14 12:43:12 +0100

    Corrección model Pokemon

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/model/Pokemon.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/model/Pokemon.kt
@@ -3,7 +3,6 @@ package com.example.pr06_lazycomponents.model
 import androidx.annotation.DrawableRes
 
 data class Pokemon(
-    val id: Int,
     val name: String,
     val type: String,
     @DrawableRes val image: Int
