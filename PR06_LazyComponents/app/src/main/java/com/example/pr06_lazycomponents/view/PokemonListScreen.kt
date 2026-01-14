cab5fcdd43148010af3bb73192426ff1aa004281
Alesa <alexandrateodora.sofronie@gracia.lasalle.cat>
2026-01-14 15:30:38 +0100

    Añadir lazy column para la lista

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/PokemonListScreen.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/PokemonListScreen.kt
@@ -6,13 +6,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.pr06_lazycomponents.model.Pokemon
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pr06_lazycomponents.viewmodel.PokemonViewModel
 
 @Composable
 fun PokemonListScreen(
     onPokemonClick: (Pokemon) -> Unit
 ) {
-    Column {
+    val viewModel: PokemonViewModel = viewModel()
+    val pokemonList = viewModel.pokemonList.value ?: emptyList()
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ){
         Text(
             text = "Pokédex",
             style = MaterialTheme.typography.headlineLarge,
@@ -20,5 +28,22 @@ fun PokemonListScreen(
                 .fillMaxWidth()
                 .padding(16.dp)
         )
+        if (pokemonList.isEmpty()) {
+            Text(
+                text = "Cargando Pokémon...",
+                modifier = Modifier.padding(16.dp)
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier.padding(horizontal = 8.dp)
+            ) {
+                items(pokemonList) { pokemon ->
+                    Text(
+                        text = pokemon.name,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
     }
 }
