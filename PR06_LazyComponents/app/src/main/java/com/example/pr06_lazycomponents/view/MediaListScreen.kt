04ff82632b8167cbe6a70bfcafac2858c22f0bf3
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2026-01-23 10:49:25 +0100

    Create MediaListScreen.kt
    Se ha creado el view MediaListScreen para mostrar una lista de Películas y Series

---
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/MediaListScreen.kt
@@ -0,0 +1,65 @@
+package com.example.pr06_lazycomponents.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.pr06_lazycomponents.model.Media
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pr06_lazycomponents.view.components.MediaItem
+import com.example.pr06_lazycomponents.viewmodel.MediaViewModel
+
+@Composable
+fun MediaListScreen(
+    onMediaClick: (Media) -> Unit,
+    viewModel: MediaViewModel
+) {
+    val viewModel: MediaViewModel = viewModel()
+    val mediaList = viewModel.mediaList.value ?: emptyList()
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(
+            text = "🎬 Películas & Series",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        if (mediaList.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Cargando contenido..."
+                )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                verticalArrangement = Arrangement.spacedBy(11.dp),
+                contentPadding = PaddingValues(bottom = 20.dp)
+
+            ) {
+                items(mediaList) { media ->
+                    MediaItem(
+                        media = media,
+                        onClick = { onMediaClick(media) }
+                    )
+                }
+            }
+        }
+    }
+}
