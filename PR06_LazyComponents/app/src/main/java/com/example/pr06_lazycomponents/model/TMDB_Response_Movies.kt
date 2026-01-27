b87bf808a36a77a726be7dec0c18c15ea7cee989
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2026-01-27 16:18:56 +0100

    COmentarios añadidos

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/model/TMDB_Response_Movies.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/model/TMDB_Response_Movies.kt
@@ -2,7 +2,7 @@ package com.example.pr06_lazycomponents.model
 
 data class TMDB_Response_Movies(
     val page: Int,
-    val results: List<Result_Movies>,
+    val results: List<Result_Movies>,       //Peliculas buscadas
     val total_pages: Int,
     val total_results: Int
 )
