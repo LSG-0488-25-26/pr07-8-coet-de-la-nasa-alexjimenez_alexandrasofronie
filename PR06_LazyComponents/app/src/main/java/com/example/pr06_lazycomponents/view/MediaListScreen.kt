35e3b96f6238ea0631fe008a207736c4ae110a5d
Alesa <alexandrateodora.sofronie@gracia.lasalle.cat>
2026-01-28 12:07:55 +0100

    Manejo de estados loading y vacio + mejoras

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/MediaListScreen.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/MediaListScreen.kt
@@ -1,4 +1,4 @@
-package com.example.pr06_lazycomponents.view
+    package com.example.pr06_lazycomponents.view
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,7 +8,9 @@ import com.example.pr06_lazycomponents.model.Media
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -21,8 +23,8 @@ fun MediaListScreen(
     onMediaClick: (Media) -> Unit,
     viewModel: MediaViewModel
 ) {
-    val viewModel: MediaViewModel = viewModel()
-    val mediaList = viewModel.mediaList.value ?: emptyList()
+    val mediaList = viewModel.mediaList.observeAsState(initial = emptyList())
+    val isLoading = viewModel.isLoading.observeAsState(initial = false)
     Column (
         modifier = Modifier.fillMaxSize()
     ){
@@ -35,31 +37,44 @@ fun MediaListScreen(
                 .fillMaxWidth()
                 .padding(16.dp)
         )
-        if (mediaList.isEmpty()) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                contentAlignment = Alignment.Center
-            ){
-                Text(
-                    text = "Cargando contenido..."
-                )
-            }
-        } else {
-            LazyColumn(
-                modifier = Modifier.padding(horizontal = 8.dp),
-                verticalArrangement = Arrangement.spacedBy(11.dp),
-                contentPadding = PaddingValues(bottom = 20.dp)
 
-            ) {
-                items(mediaList) { media ->
-                    MediaItem(
-                        media = media,
-                        onClick = { onMediaClick(media) }
+        when {
+            isLoading.value -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ){
+                    CircularProgressIndicator()
+                }
+            }
+            mediaList.value.isEmpty() -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(
+                        text = "No hay contenido disponible"
                     )
                 }
             }
+            else -> {
+                LazyColumn(
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    verticalArrangement = Arrangement.spacedBy(11.dp),
+                    contentPadding = PaddingValues(bottom = 20.dp)
+
+                ) {
+                    items(mediaList.value) { media ->
+                        MediaItem(
+                            media = media,
+                            onClick = { onMediaClick(media) }
+                        )
+                    }
+                }
         }
     }
 }
