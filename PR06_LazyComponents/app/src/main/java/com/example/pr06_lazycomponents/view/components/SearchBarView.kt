342cc70f4ebf0db0ad7197e873ac25e13b58ec5a
Alesa <alexandrateodora.sofronie@gracia.lasalle.cat>
2026-01-30 12:06:49 +0100

    Añadir icono de limpieza al search bar

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/components/SearchBarView.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/components/SearchBarView.kt
@@ -1,14 +1,17 @@
 package com.example.pr06_lazycomponents.view.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pr06_lazycomponents.viewmodel.SearchViewModel
@@ -24,10 +27,30 @@ fun SearchBarView(
     SearchBar(
         query = searchedText,
         onQueryChange = { viewModel.onSearchTextChange(it) },
-        onSearch = { onSearch(it) },
+        onSearch = {
+            viewModel.addToHistory(it)
+            onSearch(it)
+        },
         active = false,
         onActiveChange = { },
-        leadingIcon = { Icon(imageVector = Icons.Filled.Search, contentDescription = "Search") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = "Search"
+            )
+        },
+        trailingIcon = {
+            if (searchedText.isNotEmpty()) {
+                Icon(
+                    imageVector = Icons.Filled.Clear,
+                    contentDescription = "Clear",
+                    tint = Color.Red,
+                    modifier = Modifier.clickable {
+                        viewModel.clearSearchText()
+                    }
+                )
+            }
+        },
         placeholder = { Text("Buscar...") },
         modifier = Modifier
             .fillMaxWidth()
