5d428e8bd8607dce0d50f103c254c2dd454dc31c
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2026-02-05 16:27:18 +0100

    Create FavoriteScreen.kt
    Creamos la viewFavoriteScreen para agregar las películas a la pantalla Favoritos

---
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/view/FavoriteScreen.kt
@@ -0,0 +1,34 @@
+package com.example.pr06_lazycomponents.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FavoriteScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "❤️ Favoritos",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Pantalla de favoritos en desarrollo",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
