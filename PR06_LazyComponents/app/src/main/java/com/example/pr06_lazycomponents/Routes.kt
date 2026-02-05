3ac6fbb1bb2a6373879d9c150029cd45b513c1c4
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2026-02-05 16:27:38 +0100

    Update Routes.kt
    Ruta añadida para la screen Favoritos

--- PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/Routes.kt
+++ PR06_LazyComponents/app/src/main/java/com/example/pr06_lazycomponents/Routes.kt
@@ -3,4 +3,5 @@ package com.example.pr06_lazycomponents
 sealed class Screen(val route: String) {
     object ListScreen : Screen("list_screen")
     object DetailScreen : Screen("detail_screen")
+    object FavoriteScreen: Screen("favorite_screen")  //ruta pestaña favoritos
 }
